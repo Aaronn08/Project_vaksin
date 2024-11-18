72ec443ac10b03425381944aad0d006a53783817
Aaronn08 <139470627+Aaronn08@users.noreply.github.com>
2024-11-18 14:11:45 +0700

    project.sql

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Nov 2024 pada 05.55
+-- Waktu pembuatan: 18 Nov 2024 pada 08.11
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `project`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(5) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '1234', 1),
+(4, 'admin', 'd93591bdf7860e1e4ee2fca799911215', 1);
 
 -- --------------------------------------------------------
 
@@ -44,11 +65,19 @@ INSERT INTO `vaksin` (`id_vaksin`, `jenis_vaksin`, `umur`, `lokasi`, `tahun`) VA
 (2, 'cacar air', 17, 'puskesmas Sekip', 2020),
 (4, 'rubella dan DT', 7, 'puskesmas Kenten', 2024),
 (5, 'Influenza', 30, 'puskesmas Sukarami', 2015),
-(6, 'Tifoid', 20, 'puskesmas 11 ilir', 2019);
+(6, 'Tifoid', 20, 'puskesmas 11 ilir', 2019),
+(8, 'cacarapi', 12, 'puskesmas sekip', 2019),
+(19, 'cacarsapi', 23, '100ilir', 2010);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `vaksin`
@@ -61,10 +90,16 @@ ALTER TABLE `vaksin`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `vaksin`
 --
 ALTER TABLE `vaksin`
-  MODIFY `id_vaksin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_vaksin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
